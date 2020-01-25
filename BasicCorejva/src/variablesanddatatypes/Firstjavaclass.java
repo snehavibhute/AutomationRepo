707e7b21fc497f245bd423c47401d7a603bbd7a4
sneha <sneha.vibhute2017@gmail.com>
2020-01-25 12:48:00 +0530

    commiting first java class

---
+++ BasicCorejva/src/variablesanddatatypes/Firstjavaclass.java
@@ -0,0 +1,11 @@
+package variablesanddatatypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Welcome to the World of Automation");
+
+	}
+
+}
